3776df27159f45fe92983006919ae3e9b501a1b2
Andris Potapenko <andrispotapenko@gmail.com>
2023-07-24 07:48:15 +0300

    new

--- script.js
+++ script.js
@@ -41,31 +41,20 @@ bth.addEventListener('click', function() {
 })
 
 
-const view = document.querySelector('.hotels__img'); // Get view image
-const view2 = document.querySelector('.hotel__view'); // Get view2 image
+const viewDelete = document.querySelectorAll('.hotels__img'); // Get view image
+const viewAdd = document.querySelectorAll('.hotel__view'); // Get view2 image
 
-const view3 = document.querySelector('.hotels__img-2'); // Get view3 image
-const view4 = document.querySelector('.hotel__view-2'); // Get view4 image
-
-const view5 = document.querySelector('.hotels__img-3');
-const view6 = document.querySelector('.hotel__view-3');
 
 // Creating a script so that if the width is less than 1400, view and view2 get their own styles
 function checkMediaQuery() {
     if (window.innerWidth <= 1400) {
-        view.style.display = 'none';
-        view2.style.display = 'flex';
-        view3.style.display = 'none';
-        view4.style.display = 'flex';
-        view5.style.display = 'none';
-        view6.style.display = 'flex';
+        viewDelete.forEach(item => {
+            item.style.display = 'none';
+        })
     } else if (window.innerWidth >= 1400) {
-        view.style.display = 'flex';
-        view2.style.display = 'none';
-        view3.style.display = 'flex';
-        view4.style.display = 'none';
-        view5.style.display = 'flex';
-        view6.style.display = 'none';
+       viewAdd.forEach(item => {
+            item.style.display = 'flex';
+       })
     }
 }
 
